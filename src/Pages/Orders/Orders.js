f708618607030a2a8b259d2bb422f347d95ba1cb
Sachin Singh <sachinsingh020406@gmail.com>
2025-06-17 19:17:46 +0530

    Initial commit

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -177,7 +177,7 @@ export default function Orders() {
     return <div className="text-center p-6">No orders found.</div>;
 
   return (
-    <div className="max-w-4xl mx-auto p-4">
+    <div className="max-w-full mx-auto p-2">
       <h1 className="text-sm uppercase text-[#004080] font-bold mb-4">
         Your Orders
       </h1>
