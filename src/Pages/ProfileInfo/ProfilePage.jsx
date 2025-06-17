2b2f0223953fc00b3fdc4508ffae2929c94ad1d1
Sachin Singh <sachinsingh020406@gmail.com>
2025-06-17 18:58:32 +0530

    Initial commit

--- src/Pages/ProfileInfo/ProfilePage.jsx
+++ src/Pages/ProfileInfo/ProfilePage.jsx
@@ -3,6 +3,7 @@ import Orders from "../Orders/Orders";
 import Wishlist from "../Wishlist/Wishlist";
 import EditProfile from "./EditProfile";
 import ProfileInfo from "../ProfileInfo/ProfileInfo"; // Import ProfileInfo component
+import { ToastContainer } from "react-toastify";
 
 export default function ProfilePage() {
   const [activeTab, setActiveTab] = useState("profile");
@@ -42,6 +43,7 @@ export default function ProfilePage() {
         {activeTab === "wishlist" && <Wishlist />}
         {activeTab === "edit" && <EditProfile />}
       </div>
+      <ToastContainer position="top-right" autoClose={3000} hideProgressBar />
     </div>
   );
 }
